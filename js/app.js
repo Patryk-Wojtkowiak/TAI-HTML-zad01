c6fd0bce9512198b2fcf6bcd81a31f64dcf8edf8
Patryk-Wojtkowiak <patryk.wojtkowiak.ti15@zs1-swarzedz.pl>
2017-03-23 14:16:50 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,4 +5,27 @@ window.addEventListener('DOMContentLoaded', function() {
   a[0].style.display = 'none';
   a[1].style.display = 'block';
   a[2].style.display = 'none';
-  console.log(a)});
+  console.log(a);
+  console.log('napis', 2+2);
+
+  for (var i = 0; i < a.length; i++) {
+    a[i].addEventListener('click', function() {
+      console.log('click');
+      if (a_low == false) {
+        a[0].style.display = 'block';
+        a[1].style.display = 'none';
+        a[2].style.display = 'none';
+        a_low = true;
+        a_high = false;
+      } else {
+        a[0].style.display = 'none';
+        a[1].style.display = 'block';
+        a[2].style.display = 'none';
+        a_low = false;
+        a_high = true;
+
+        }
+      });
+    }
+  });
+
